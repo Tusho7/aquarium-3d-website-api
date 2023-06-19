577d2d29c484117c00015a48ec6ea5db90b234be
Tusho7 <sandrotushurashvili7@gmail.com>
2023-06-19 15:23:09 +0400

    feat: Create Fish model

---
+++ src/models/Fish.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const fishSchema = new Schema({
+  name: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  character: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  watertemp: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  about: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  size: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  parameters: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  lifespan: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  minimumTankSize: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  food: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  images: {
+    png: {
+      type: Schema.Types.String,
+      required: true,
+    },
+  },
+  id: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+});
+
+const Fish = mongoose.model("Fish", fishSchema);
+
+export default Fish
