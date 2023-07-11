4b49b1d97e389949a98eb9458a481576bc63829e
Tusho7 <sandrotushurashvili7@gmail.com>
2023-07-11 17:58:17 +0400

    feat: Create map on plant model

--- src/controllers/plantcontroller.js
+++ src/controllers/plantcontroller.js
@@ -2,5 +2,19 @@ import Plant from "../models/Plant.js";
 
 export const getAllPlants = async(req,res) => {
     const data = await Plant.find();
-    return res.json(data);
+    const newData = data.map((plant) => {
+        return {
+            id: plant.id,
+            name: plant.name,
+            co2: plant.co2,
+            growthRate: plant.growthRate,
+            care: plant.care,
+            light: plant.light,
+            about: plant.about,
+            size: plant.size,
+            parameters: plant.parameters,
+            images: plant.images
+        }
+    })
+    return res.json(newData);
 }
