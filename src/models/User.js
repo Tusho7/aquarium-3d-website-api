6d958a3e6251d67fe3afce9c21cdb667f794c071
Tusho7 <sandrotushurashvili7@gmail.com>
2023-07-13 14:39:51 +0400

    feat: Create User model

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose  from "mongoose";
+mongoose.set("strictQuery", false);
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
