c47da38cec481a843b63d95ab63b539538799f58
Tusho7 <sandrotushurashvili7@gmail.com>
2023-07-17 22:35:03 +0400

    feat: Implement password restrictions for signup

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -3,78 +3,104 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 
-export const signup = async (req,res) => {
-    const { email, password } = req.body;
-    const { file } = req;
+export const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const { file } = req;
 
-    if(!email || !password) {
-        return res.status(400).json({ error: "Enter all required fields" });
-    }
+  if (!email || !password) {
+    return res.status(400).json({ error: "Enter all required fields" });
+  }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailRegex.test(email)) {
-        return res.status(400).json({ error: "Invalid email format" });
-    }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
 
-    const exist = await User.findOne({ email });
-    if(exist) {
-        return res.status(400).json({ error: "An account with this email already exists" });
-    }
+  const exist = await User.findOne({ email });
+  if (exist) {
+    return res
+      .status(400)
+      .json({ error: "An account with this email already exists" });
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ error: "Password should be at least 8 characters" });
+  }
 
-    const newUser = new User({
-        email,
-        password: hashedPassword,
-        avatar: "avatars/" + file.originalname,
-        id: uuidv4(),
-    });
+  if (!/[a-z]/.test(password)) {
+    return res
+      .status(400)
+      .json({ error: "Password should contain at least one lowercase" });
+  }
 
-    const saveUser = await newUser.save();
+  if (!/[A-Z]/.test(password)) {
+    return res
+      .status(400)
+      .json({ error: "Password should contain at least one uppercase" });
+  }
 
-    res.status(201).send({ user: saveUser });
+  if (!/\d/.test(password)) {
+    return res
+      .status(400)
+      .json({ error: "Password should contain at least one number" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    email,
+    password: hashedPassword,
+    avatar: "avatars/" + file.originalname,
+    id: uuidv4(),
+  });
+
+  const saveUser = await newUser.save();
+
+  res.status(201).send({ user: saveUser });
 };
 
-export const loginUser = async (req,res) => {
-    const { email, password } = req.body;
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-    if(!email || !password) {
-        return res.status(400).json({ error: "Enter all required fields" });
-    }
+  if (!email || !password) {
+    return res.status(400).json({ error: "Enter all required fields" });
+  }
 
-    const existingUser = await User.findOne({ email });
-    if(!existingUser) {
-        return res.status(400).json({ error: "Wrong email or password" });
-    }
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    return res.status(400).json({ error: "Wrong email or password" });
+  }
 
-    const isValidPassword = await bcrypt.compare(password, existingUser.password);
-    if(!isValidPassword) {
-        return res.status(401).json({ error: "Wrong email or password "});
-    }else{
-        const token = jwt.sign(
-            {
-                id: existingUser.id,
-                email: existingUser.email,
-                avatar: existingUser.avatar,
-            },
-            process.env.JWT_SECRET_KEY
-        );
+  const isValidPassword = await bcrypt.compare(password, existingUser.password);
+  if (!isValidPassword) {
+    return res.status(401).json({ error: "Wrong email or password " });
+  } else {
+    const token = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+        avatar: existingUser.avatar,
+      },
+      process.env.JWT_SECRET_KEY
+    );
 
-        return res.status(201).json({
-            message: "Login Successfully",
-            token,
-        });
-    }
+    return res.status(201).json({
+      message: "Login Successfully",
+      token,
+    });
+  }
 };
 
 export const authenticate = (req, res, next) => {
-    const auth = req.header("Authorization");
-  
-    if (!auth) {
-      return res.status(401).send("Access denied. No token provided");
-    }
-  
-    const [, token] = auth.trim().split(" ");
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    return res.status(200).json(decoded);
-  };
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    return res.status(401).send("Access denied. No token provided");
+  }
+
+  const [, token] = auth.trim().split(" ");
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  return res.status(200).json(decoded);
+};
